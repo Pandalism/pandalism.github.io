be6d6f1e1cd02774e9e48ca7f1d9ce8a8d5146e4
Pandalism <f.diaz-maroto@hotmail.com>
2019-03-05 23:27:23 +0000

    Open and close menu js and css done

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+// jquery apparently likes to lie in here. makes sure document is fully loaded
+// before triggering anything
+
+
+
+// If nav icon is clicked, toggle drop down menu
+$("img.nav_icon").click(function(){
+  console.log("mouse clicked!");
+  if (checkMobile() )   {
+    toggleMenu()
+  }
+  })
+
+});
+
+
+
+
+// /* Functions */
+// check to see if website is in mobile mode or not by checking if the
+// main container is in flex mode or not.
+function checkMobile() {
+  if ($(".body-container").css("display") == "flex") {
+    var isMobile = false;
+    // $('p.debugger').text("isNotMobile")
+  } else {
+    var isMobile = true;
+    // $('p.debugger').text("isMobile")
+  }
+
+  return isMobile
+};
+//
+//
+function  toggleMenu() {
+    $(".hidden-menu").toggleClass("hide-menu")
+  return
+};
